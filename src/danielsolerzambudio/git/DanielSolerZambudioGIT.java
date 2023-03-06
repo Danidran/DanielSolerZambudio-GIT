edb30e50585890c42dacc1145a1d26a94ccad4ce
Daniel <Daniel@DESKTOP-39OJL9U>
2023-03-06 23:05:33 +0100

    x=9

--- src/danielsolerzambudio/git/DanielSolerZambudioGIT.java
+++ src/danielsolerzambudio/git/DanielSolerZambudioGIT.java
@@ -15,7 +15,7 @@ public class DanielSolerZambudioGIT {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int x=4;
+        int x=9;
         System.out.println(x);
     }
     
