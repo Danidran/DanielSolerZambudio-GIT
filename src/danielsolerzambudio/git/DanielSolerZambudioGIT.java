670323cb24ddd5fcdcb6004a7d27f150521988ba
Daniel <Daniel@DESKTOP-39OJL9U>
2023-03-06 16:57:46 +0100

    Versión 1.

---
+++ src/danielsolerzambudio/git/DanielSolerZambudioGIT.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package danielsolerzambudio.git;
+
+/**
+ *
+ * @author Daniel
+ */
+public class DanielSolerZambudioGIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x=1;
+        System.out.println(x);
+    }
+    
+}
